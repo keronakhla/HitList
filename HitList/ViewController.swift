b86077bfbea2f39f88911fb8d32587ce07a2f687
Kerolos <keronakhla@gmail.com>
2015-11-29 22:03:32 -0800

    Save to Core Data

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -7,21 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
     // names is a mutable array to hold strings for the table view to display
-    var names = [String]()
+    var people = [NSManagedObject]()
     
     @IBAction func addName(sender: AnyObject) {
         let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .Alert)
         
-        let saveAction = UIAlertAction(title: "Save", style: .Default, handler: { (action:UIAlertAction) -> Void in
+        let saveAction = UIAlertAction(title: "Save", style: .Default, handler:{ (action:UIAlertAction) -> Void in
             
             let textField = alert.textFields!.first
-            self.names.append(textField!.text!)
+            self.saveName(textField!.text!)
             self.tableView.reloadData()
         })
         
@@ -48,17 +49,42 @@ class ViewController: UIViewController, UITableViewDataSource {
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
+    func saveName(name: String){
+        // 1
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        // 2
+        let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext)
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        // 3
+        person.setValue(name, forKey: "name")
+        
+        // 4
+        do {
+            try managedContext.save()
+        // 5
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
     // MARK: UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
-        cell!.textLabel!.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        
+        cell!.textLabel!.text = person.valueForKey("name") as? String
         
         return cell!
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
